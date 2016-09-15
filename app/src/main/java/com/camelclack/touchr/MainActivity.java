c87daf1bd22b095c33ae5eefcee088853591b399
Atlee Brink <atlee@atleebrink.com>
2016-09-15 02:09:52 -0700

    fast nums

--- app/src/main/java/com/camelclack/touchr/MainActivity.java
+++ app/src/main/java/com/camelclack/touchr/MainActivity.java
@@ -1,19 +1,37 @@
 package com.camelclack.touchr;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+  private static final String KEY_NUM = "num";
+
+  private long num = 1;
+
+  // Create an anonymous implementation of OnClickListener
+  private View.OnClickListener mClickListener = new View.OnClickListener() {
+    public void onClick(View v) {
+      num *= 10;
+      showNum();
+    }
+  };
+
+  private void showNum() {
+    TextView text = (TextView)findViewById(R.id.number);
+    text.setText("" + num);
+  }
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
     setContentView(R.layout.activity_main);
+
     TextView text = (TextView)findViewById(R.id.number);
-    text.setText("0");
+    text.setOnClickListener(mClickListener);
   }
 
-  protected void onClickThing() {
-
-  }
 }
